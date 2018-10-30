626063b024e3c1bd663927c24718c7ba3ead2895
yuliya severyukhina <yuliya.severyukhina.94@mail.ru>
2018-10-30 03:32:58 +0300

    123

--- EM.Calc.ConsoleApp/EM.Calc.ConsoleApp/Program.cs
+++ EM.Calc.ConsoleApp/EM.Calc.ConsoleApp/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EM.Calc.ConsoleApp
 {
@@ -10,7 +7,55 @@ namespace EM.Calc.ConsoleApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите строку для выполнения, например: sum 2 2 2...");
+            string text = Console.ReadLine();
+            string[] words = text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            int[] numbers = new int[words.Length - 1];
 
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = Convert.ToInt32(words[i + 1]);
+            }
+
+            string operation = words[0];
+            switch (operation)
+            {
+                case ("sum"):
+                    Console.WriteLine("sum = " + Sum(numbers));
+                    break;
+                case ("sub"):
+                    Console.WriteLine("sub = " + Sub(numbers));
+                    break;
+                case ("pow"):
+                    Console.WriteLine("pow = " + Pow(numbers));
+                    break;
+            }
+            Console.ReadKey();
+        }
+
+        public static int Sum(int[] args)
+        {
+            return args.Sum();
+        }
+
+        public static int Sub(int[] args)
+        {
+            int res = args[0] - args[1];
+            for (int i = 2; i < args.Length; i++)
+            {
+                res = res - args[i];
+            }
+            return res;
+        }
+
+        public static int Pow(int[] args)
+        {
+            int res = (int)Math.Pow(args[0], args[1]);
+            for (int i = 2; i < args.Length; i++)
+            {
+                res = (int)Math.Pow(res, args[i]);
+            }
+            return res;
         }
     }
 }
