6bf2453df8d8e6db6586c2660487e6069cf1b8d4
yuliya severyukhina <yuliya.severyukhina.94@mail.ru>
2018-10-30 15:11:37 +0300

    add usability remove tests add new operation

--- EM.Calc.ConsoleApp/EM.Calc.ConsoleApp/Program.cs
+++ EM.Calc.ConsoleApp/EM.Calc.ConsoleApp/Program.cs
@@ -7,7 +7,7 @@ namespace EM.Calc.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите строку для выполнения, например: sum 2 2 2...");
+            /*Console.WriteLine("Введите строку для выполнения, например: sum 2 2 2...");
             string text = Console.ReadLine();
             string[] words = text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
             int[] numbers = new int[words.Length - 1];
@@ -15,47 +15,64 @@ namespace EM.Calc.ConsoleApp
             for (int i = 0; i < numbers.Length; i++)
             {
                 numbers[i] = Convert.ToInt32(words[i + 1]);
+            }*/
+
+            string[] operations = new[] { "sum", "sub", "pow", "new" };
+
+            string operation, operands;
+            double[] values;
+
+            var calc = new Core.Calc();
+
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Список операций: ");
+
+                foreach (var item in operations)
+                {
+                    Console.WriteLine(item);
+                }
+
+                Console.WriteLine("Введите операцию ");
+                operation = Console.ReadLine();
+
+                Console.WriteLine("Введите операнды: ");
+                operands = Console.ReadLine();
+                values = ConvertToDouble(operands.Split(new[] { " ", ";" }, StringSplitOptions.RemoveEmptyEntries));
             }
 
-            string operation = words[0];
+            else
+            {
+                operation = args[0].ToLower();
+                values = ConvertToDouble(args, 1);
+
+            }
+          
             switch (operation)
             {
-                case ("sum"):
-                    Console.WriteLine("sum = " + Sum(numbers));
+                /*case ("sum"):
+                    Console.WriteLine("sum = " + calc.Sum(values));
                     break;
                 case ("sub"):
-                    Console.WriteLine("sub = " + Sub(numbers));
+                    Console.WriteLine("sub = " + calc.Sub(values));
                     break;
                 case ("pow"):
-                    Console.WriteLine("pow = " + Pow(numbers));
+                    Console.WriteLine("pow = " + calc.Pow(values));
+                    break;*/
+                case ("new"):
+                    Console.WriteLine(calc.New(values));
                     break;
             }
             Console.ReadKey();
         }
 
-        public static int Sum(int[] args)
+        private static double[] ConvertToDouble(string[] args, int start = 0)
         {
-            return args.Sum();
-        }
-
-        public static int Sub(int[] args)
-        {
-            int res = args[0] - args[1];
-            for (int i = 2; i < args.Length; i++)
-            {
-                res = res - args[i];
-            }
-            return res;
-        }
-
-        public static int Pow(int[] args)
-        {
-            int res = (int)Math.Pow(args[0], args[1]);
-            for (int i = 2; i < args.Length; i++)
-            {
-                res = (int)Math.Pow(res, args[i]);
-            }
-            return res;
+            return args
+                .ToList()
+                .Skip(start)
+                .Select(Convert.ToDouble)
+                .ToArray();
         }
     }
 }
