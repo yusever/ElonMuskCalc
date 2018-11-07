cd2919dcd919f4a8465d1f8660dad6210c6fa57d
yuliya severyukhina <yuliya.severyukhina.94@mail.ru>
2018-11-07 13:53:04 +0300

    123

--- EM.Calc.ConsoleApp/EM.Calc.Web/App_Start/BundleConfig.cs
+++ EM.Calc.ConsoleApp/EM.Calc.Web/App_Start/BundleConfig.cs
@@ -22,6 +22,9 @@ namespace EM.Calc.Web
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/elonmusk.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
